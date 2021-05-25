475fcee31e9b5172e0626dc6e985c77075a92417
KhoirulAziz23 <84756729+KhoirulAziz23@users.noreply.github.com>
2021-05-26 03:45:30 +0700

    Update index.js

--- api/index.js
+++ api/index.js
@@ -6,7 +6,7 @@ const model = require('./sdk/model.js');
 
 // Bot Setting
 const TelegramBot = require('node-telegram-bot-api');
-const token = '1733547356:AAEOX7oG_z09vS34M-DUHOm5YCPsXYDXohg'
+const token = '1817108964:AAGGAi5_Fq4K1xQi9dRjfMN9WePU4p7RVVY'
 const bot = new TelegramBot(token, {polling: true});
 
 
